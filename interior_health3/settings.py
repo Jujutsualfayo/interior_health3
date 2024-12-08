42315e71ca590daaca05396c564cd32de09002b0
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-08 17:33:16 +0000

    updated

--- interior_health3/settings.py
+++ interior_health3/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "interior_health3.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'interiorhealth3',
+        'USER': 'interior_health_user',  # Your new MySQL username
+        'PASSWORD': 'Alphafemale1',      # The password you set
+        'HOST': 'localhost',             # Use 'localhost' for local development
+        'PORT': '3306',                  # Default MySQL port
     }
 }
 
