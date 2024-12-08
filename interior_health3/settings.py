757f488af2adaf99ba3e181f381b2e274c207499
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-08 10:04:18 +0000

    updated

--- interior_health3/settings.py
+++ interior_health3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hm&!i8p&)zjgiazp7%b!e+1lx1)m55jc+yo3yw!--(0$$ba40n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
