788edafc5a9c60761901331f2de1ed084158db15
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-08 15:18:12 +0000

    updated

--- interior_health3/urls.py
+++ interior_health3/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin panel
+    path('', user_views.home, name='home'), 
     path('', include('users.urls')),  # Include URLs from the `users` app
 ]
