a746e6937f60cb19f6cea631bbaa4b915a86319a
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-10 18:43:00 +0000

    updated

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from drugs.models import Drug
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('DELIVERED', 'Delivered'),
+        ('CANCELED', 'Canceled'),
+    ]
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    order_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user.username}"
+
