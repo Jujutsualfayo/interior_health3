7aa42fc95ec274b024090d0f46edafbbabbd0105
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-14 16:54:44 +0000

    rnoreverse match fix

--- interior_health3/urls.py
+++ interior_health3/urls.py
@@ -4,9 +4,7 @@ from users import views as user_views  # Import views from the users app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', user_views.home, name='home'),  # Global home view
     path('users/', include('users.urls')),
     path('drugs/', include('drugs.urls')),
     path('orders/', include('orders.urls')),  
-    
 ]
