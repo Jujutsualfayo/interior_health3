0c2a375ac20c7f627d2b988b1db7867aca2b1ccb
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-14 17:41:14 +0000

    updated views.orderlist

--- orders/urls.py
+++ orders/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('order/list/', views.order_list, name='order_list'),
+    path('list/', views.order_list, name='order_list'),
+    path('place/<int:drug_id>/', views.place_order, name='place_order'),
     path('<int:pk>/', views.order_detail, name='order_detail'),
     path('<int:pk>/cancel/', views.cancel_order, name='cancel_order'),
-    path('place/<int:drug_id>/', views.place_order, name='place_order'),
 ]
-
