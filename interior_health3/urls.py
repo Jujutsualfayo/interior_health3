252087c67dc8def66f305a9f5d572ed45883de35
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-15 10:01:05 +0000

    set path for global home

--- interior_health3/urls.py
+++ interior_health3/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from users import views as user_views  # Import views from the users app
 
 urlpatterns = [
+    path('', global_home, name='global_home'),  # Root URL for the global home page
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('drugs/', include('drugs.urls')),
