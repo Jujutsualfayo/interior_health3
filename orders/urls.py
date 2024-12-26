c9c726d456f843d16d8e9628164d43699f6268f8
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-26 10:12:14 +0000

    general order placement

--- orders/urls.py
+++ orders/urls.py
@@ -5,8 +5,8 @@ app_name = 'orders'
 
 urlpatterns = [
     path('list/', views.order_list, name='order_list'),
-    path('place/', views.place_order, name='place_order'),  # New path for ordering without a specific drug
-    path('place/<int:drug_id>/', views.place_order, name='place_order_with_drug'),  # Existing path for specific drug orders
+    path('place/', views.place_order, name='place_order'),  # For general order placement
+    path('place/<int:drug_id>/', views.place_order, name='place_order_with_drug'),  # For order placement with a specific drug
     path('<int:pk>/', views.order_detail, name='order_detail'),
     path('<int:pk>/cancel/', views.cancel_order, name='cancel_order'),
 ]
