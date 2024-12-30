11df5ffc446cda0899aca066fc9bc4dd81de860f
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-30 22:25:04 +0000

    updated place order btn

--- drugs/urls.py
+++ drugs/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'drugs'  # 
+app_name = 'drugs'   
 
 urlpatterns = [
     path('', views.drug_list, name='drug_list'), 
--- orders/urls.py
+++ orders/urls.py
@@ -5,9 +5,10 @@ app_name = 'orders'
 
 urlpatterns = [
     path('list/', views.order_list, name='order_list'),
-    path('place/', views.place_order, name='place_order'),  # For general order placement
-    path('place/<int:drug_id>/', views.place_order, name='place_order_with_drug'),  # For order placement with a specific drug
+    path('place/', views.place_order, name='place_order'),  # General order placement
+    path('place/drug/<int:drug_id>/', views.place_order, name='place_order_with_drug'),  # Specific drug order
     path('<int:pk>/', views.order_detail, name='order_detail'),
     path('history/', views.order_history, name='order_history'),
     path('<int:pk>/cancel/', views.cancel_order, name='cancel_order'),
 ]
+
