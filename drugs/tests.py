79ff3672e3b9dbd14cbeedcb3f8eb7abb23ffde1
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-01 18:48:04 +0000

    update drug list view

--- drugs/tests.py
+++ drugs/tests.py
@@ -89,3 +89,16 @@ class DrugViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No drugs available.")  # This should pass if the template is correct
 
+from django.test import TestCase
+from django.urls import reverse
+from drugs.models import Drug
+
+class DrugListViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.client.login(username='testuser', password='password123')
+
+    def test_drug_list_view(self):
+        response = self.client.get(reverse('drugs:drug_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'drugs/drug_list.html')
