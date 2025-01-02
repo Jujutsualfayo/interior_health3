865b1c86bd5f76163c5afa4d09e7913f82c94f19
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-02 20:14:22 +0000

    import profile

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+from .models import Profile
 
 
 def home(request):
@@ -43,6 +44,10 @@ def logout_view(request):
 
 @login_required
 def profile(request):
+    # Ensure the user has a profile
+    if not hasattr(request.user, 'profile'):
+        Profile.objects.create(user=request.user)
+
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
@@ -54,8 +59,9 @@ def profile(request):
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
-    
+
     return render(request, 'users/profile.html', {
         'user_form': user_form,
         'profile_form': profile_form
     })
+
