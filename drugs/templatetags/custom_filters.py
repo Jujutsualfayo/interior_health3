aeb4b2302b3d6f86a157250ec86cf7035e7bc0e6
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-03 09:52:09 +0000

    updated drug list view

--- drugs/templatetags/custom_filters.py
+++ drugs/templatetags/custom_filters.py
@@ -2,8 +2,13 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='is_admin')
+@register.filter
 def is_admin(user):
-    """Check if a user belongs to the 'Admin' group."""
+    """Check if the user belongs to the Admin group."""
     return user.groups.filter(name='Admin').exists()
+
+@register.filter
+def is_patient(user):
+    """Check if the user belongs to the Patient group."""
+    return user.groups.filter(name='Patient').exists()
 
