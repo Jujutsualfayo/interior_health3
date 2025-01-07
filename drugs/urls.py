1f43e7f86d7d823b08077962916de5566a1cb43d
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-07 19:20:46 +0000

    drug_list_view for the web view

--- drugs/urls.py
+++ drugs/urls.py
@@ -1,9 +1,9 @@
-# drugs/urls.py
 from django.urls import path
-from .views import drug_list_api
+from .views import drug_list_api, drug_list_view
 
 app_name = 'drugs'
 
 urlpatterns = [
     path('api/drugs/', drug_list_api, name='drug_list_api'),
+    path('drugs/', drug_list_view, name='drug_list'),  # New URL for the web view
 ]
