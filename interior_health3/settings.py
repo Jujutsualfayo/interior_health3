11b83bcaa60f3ae149876c1129778cf9fb0e6987
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-13 19:07:45 +0000

    updated

--- interior_health3/settings.py
+++ interior_health3/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = "interior_health3.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'interior_health_app',
+        'USER': 'jujutsualfayo',
+        'PASSWORD': 'alphafemale1',
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
