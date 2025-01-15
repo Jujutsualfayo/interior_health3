b654271998a9103526359ac740d7015cb268b353
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-15 18:37:41 +0000

    updated urls.py drugs

--- drugs/urls.py
+++ drugs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import drug_list_api, drug_list_view
+from .views import drug_list_api, drug_detail_api, drug_list_view
 
 app_name = 'drugs'
 
 urlpatterns = [
-    path('api/drugs/', drug_list_api, name='drug_list_api'),
-    path('drugs/', drug_list_view, name='drug_list'),  # New URL for the web view
+    path('api/drugs/', drug_list_api, name='drug_list_api'),  # List and create drugs
+    path('api/drugs/<int:pk>/', drug_detail_api, name='drug_detail_api'),  # Retrieve, update, delete a single drug
+    path('drugs/', drug_list_view, name='drug_list'),  # Web view for the list of drugs
 ]
