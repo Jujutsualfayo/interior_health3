f4d665fa308378eef45b887a3ca245cd74e8a79e
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-16 08:46:19 +0000

    updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,7 @@ import DrugList from '../components/DrugList.js';
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center my-8">Welcome to the Interior Health App</h1>
+      <h1 className="text-3xl font-bold text-center my-8">Welcome to  Interior Health </h1>
       <DrugList />
     </div>
   );
