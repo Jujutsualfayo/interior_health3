dab428c43af46f114bf1e0ea8d167e2fde443b65
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-16 07:21:35 +0000

    update on navbar.js ext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Navbar from './components/Navbar.js';  // Add .js extension
+import Home from './pages/Home.js';           // Add .js extension
+import Login from './pages/Login.js';         // Add .js extension
 
 const App = () => {
   return (
