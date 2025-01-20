17c13bfc60e2babe1f0a8c625829e5eec4b0ce58
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-20 19:19:37 +0000

    updated orders.models

--- drugs/views.py
+++ drugs/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Drug
-from .models import Order, Drug
+from orders.models import Order
 from .serializers import DrugSerializer
 
 
