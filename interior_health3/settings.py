1d8d1f0449cfd2c75e38f61d7cb02fea26138d5b
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-01-23 08:58:49 +0000

    updated

--- interior_health3/settings.py
+++ interior_health3/settings.py
@@ -1,8 +1,12 @@
 import os
 from pathlib import Path
+import dj_database_url
+import django_heroku
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+django_heroku.settings(locals())
 
 # Secret key from environment variable
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-key-for-dev")  # Replace in production!
@@ -94,10 +98,7 @@ CHANNEL_LAYERS = {
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 # Authentication and Password Validation
